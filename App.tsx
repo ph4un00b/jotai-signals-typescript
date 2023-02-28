d856b114ff6ddf0a31b3b650f6f679cd92be9cb8
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-02-28 14:07:28 -0600

    jotai-sample

--- App.tsx
+++ App.tsx
@@ -1,11 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button } from "react-native";
+import { atom } from "jotai/vanilla";
+import { useAtom, useSetAtom } from "jotai/react";
+import { $, createElement } from "jotai-signal";
+
+const countAtom = atom(0);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>React Native Example</Text>
+      <View>
+        <Controls />
+        <Counter />
+        <CounterWithSignal />
+      </View>
+    </View>
+  );
+}
+
+function Counter() {
+  const [count] = useAtom(countAtom);
+  return (
+    <View>
+      <Text style={styles.h1}>With useAtom(atom)</Text>
+
+      <Text style={styles.p}>
+        Count: {count} ({Math.random()})
+      </Text>
+    </View>
+  );
+}
+
+function CounterWithSignal() {
+  return (
+    <View>
+      <Text style={styles.h1}>With $(atom)</Text>
+
+      {createElement(
+        Text,
+        { style: styles.p },
+        "Count: ",
+        $(countAtom),
+        " (",
+        Math.random(),
+        ")"
+      )}
+    </View>
+  );
+}
+
+function Controls() {
+  const setCount = useSetAtom(countAtom);
+  return (
+    <View>
+      <Button onPress={() => setCount((c) => c + 1)} title="increment" />
     </View>
   );
 }
@@ -13,8 +61,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8
   },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  p: {
+    margin: 24,
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  h1: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
 });
