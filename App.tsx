b0b81f0bd9ca169c7dfbc6dde112605011a31b03
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-02-28 20:01:30 -0600

    format

--- App.tsx
+++ App.tsx
@@ -6,93 +6,93 @@ import { $, createElement } from "jotai-signal";
 const countAtom = atom(0);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>React Native Example</Text>
-      <View>
-        <Controls />
-        <Counter />
-        <CounterWithHandCompiledSignal />
-        <CounterWithSignal />
-      </View>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.paragraph}>React Native Example</Text>
+			<View>
+				<Controls />
+				<Counter />
+				<CounterWithHandCompiledSignal />
+				<CounterWithSignal />
+			</View>
+		</View>
+	);
 }
 
 function Counter() {
-  const [count] = useAtom(countAtom);
-  return (
-    <View>
-      <Text style={styles.h1}>With useAtom(atom)</Text>
+	const [count] = useAtom(countAtom);
+	return (
+		<View>
+			<Text style={styles.h1}>With useAtom(atom)</Text>
 
-      <Text style={styles.p}>
-        Count: {count} ({Math.random()})
-      </Text>
-    </View>
-  );
+			<Text style={styles.p}>
+				Count: {count} ({Math.random()})
+			</Text>
+		</View>
+	);
 }
 
 function CounterWithHandCompiledSignal() {
-  return (
-    <View>
-      <Text style={styles.h1}>With createElement($(atom))</Text>
+	return (
+		<View>
+			<Text style={styles.h1}>With createElement($(atom))</Text>
 
-      {createElement(
-        Text,
-        { style: styles.p },
-        "Count: ",
-        $(countAtom),
-        " (",
-        Math.random(),
-        ")"
-      )}
-    </View>
-  );
+			{createElement(
+				Text,
+				{ style: styles.p },
+				"Count: ",
+				$(countAtom),
+				" (",
+				Math.random(),
+				")"
+			)}
+		</View>
+	);
 }
 
 function CounterWithSignal() {
-  return (
-    <View>
-      <Text style={styles.h1}>With $(atom)</Text>
-      <Text style={styles.p}>
-        Count: {$(countAtom)} ({Math.random()})
-      </Text>
-    </View>
-  );
+	return (
+		<View>
+			<Text style={styles.h1}>With $(atom)</Text>
+			<Text style={styles.p}>
+				Count: {$(countAtom)} ({Math.random()})
+			</Text>
+		</View>
+	);
 }
 
 function Controls() {
-  const setCount = useSetAtom(countAtom);
-  return (
-    <View>
-      <Button onPress={() => setCount((c) => c + 1)} title="increment" />
-    </View>
-  );
+	const setCount = useSetAtom(countAtom);
+	return (
+		<View>
+			<Button onPress={() => setCount((c) => c + 1)} title="increment" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-    padding: 8
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  p: {
-    margin: 24,
-    fontSize: 14,
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  h1: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center"
-  }
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		backgroundColor: "#ecf0f1",
+		padding: 8
+	},
+	paragraph: {
+		margin: 24,
+		fontSize: 18,
+		fontWeight: "bold",
+		textAlign: "center"
+	},
+	p: {
+		margin: 24,
+		fontSize: 14,
+		fontWeight: "bold",
+		textAlign: "center"
+	},
+	h1: {
+		margin: 24,
+		fontSize: 18,
+		fontWeight: "bold",
+		textAlign: "center"
+	}
 });
--- readme.md
+++ readme.md
@@ -4,15 +4,23 @@
 ##### info
 - [Demystifying Create React Signals Internals](https://blog.axlight.com/posts/demystifying-create-react-signals-internals/)
 
+##### worked on
+- [x] javascript (npx create-expo-app my-app)
+- [x] typescript (sdk 48, npx create-expo-app --template)
+- [x] web (thru react-native-web)
+- [x] dev builds (https://docs.expo.dev/development/create-development-builds/)
+
+##### todo
+- [ ] preview mode (npx expo prebuild && npx expo run:android)
+
 ##### branches
-    - master
-        - npx expo start --android
-        - npx expo start --ios
-    - web
-        - npx expo start --web
+	- master (typescript)
+		- npx expo start --android
+		- npx expo start --ios
+	- web
+		- npx expo start --web
 
 ##### online samples (by Mr. Daishi)
-
 - https://snack.expo.dev/kjclneke1
 - https://codesandbox.io/s/priceless-star-kg7dmh?file=/src/App.js
 
